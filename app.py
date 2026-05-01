d5dc5c36ac92b88179b8112eb82f4ebc3ebe1f59
Jivitesh Gudekar <gudekar.jivitesh@gmail.com>
2026-05-01 14:30:42 -0300

    Agents class create

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 if GROQ_API_KEY is None:
     raise RuntimeError("GROQ_API_KEY not found in .env")
+
+class Agent:
+    def __init__(self, client, system):
+        self.client = client
+        self.system = system
+        self.messages = []
+
+        if self.system is not None:
+                self.messages.append({"role": "system", "content": self.system})
+
+    def __call__(self,message):
+         if message:
+              self.messages.append({"role": "user", "content": message})

a8dd952c3ebfecf80968435cca4cbe6d040e864b
Jivitesh Gudekar <gudekar.jivitesh@gmail.com>
2026-05-01 15:06:28 -0300

    agent class created

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from groq import Groq
 
 load_dotenv()
 
@@ -7,6 +8,21 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 if GROQ_API_KEY is None:
     raise RuntimeError("GROQ_API_KEY not found in .env")
+
+client = Groq(
+     api_key=GROQ_API_KEY
+)
+
+# chat_completion = client.chat.completions.create(
+#     model = "llama3-70b-instruct",
+#     messages = [
+#         {"role": "user", 
+#          "content": "Explain the advantage of fast language models."},
+#         {"role": "system", "content": "You are a helpful assistant."},
+#     ]
+# )
+
+# print(chat_completion.choices[0].message.content)
 
 class Agent:
     def __init__(self, client, system):
@@ -18,5 +34,15 @@ class Agent:
                 self.messages.append({"role": "system", "content": self.system})
 
     def __call__(self,message):
-         if message:
-              self.messages.append({"role": "user", "content": message})
+        if message:
+            self.messages.append({"role": "user", "content": message})
+        result = self.execute()
+        self.messages.append({"role": "assistant", "content": result})
+        return result
+    
+    def execute(self):
+        completion = self.client.chat.completions.create(
+            model = "llama3-70b-instruct",
+            messages = self.messages
+        )
+        return completion.choices[0].message.content
